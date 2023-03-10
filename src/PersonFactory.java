c8b6598656bd5d3f6551441a5f70fa296b16c8dd
carolynh2025 <carolynh2025@student.cis.edu.hk>
2023-03-10 08:52:41 +0800

    h

--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -9,14 +9,40 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
+        if (allPersons.size() == 0){
+            allPersons.add(person);
+        }
+
+        for(int i = 0; i < allPersons.size(); i++){
+            if(person.getLastName().compareTo(allPersons.get(i).getLastName()) < 0) {
+                allPersons.add(i, person);
+            }
+            else{
+                allPersons.add(person);
+            }
+        }
 
     }
 
     public ArrayList<Person> under18() {
+        ArrayList<Person> under18 = new ArrayList<>();
+        for(int i = 0; i < allPersons.size(); i++){
+            if(allPersons.get(i).getAge()<18){
+                under18.add(allPersons.get(i));
+
+            }
+        }
+        return under18;
 
     }
 
     public boolean checkLastName(String lastName) {
+        for(int i = 0; i < allPersons.size(); i++){
+            if(allPersons.get(i).getLastName().equals(lastName)){
+                return true;
+            }
+        }
+        return false;
 
     }
 }
